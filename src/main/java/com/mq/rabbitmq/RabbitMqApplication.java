22faeecb63bc97a74132622666b8d340d0d25915
heyu <heyu@qq.com>
2020-03-06 00:36:09 +0800

    update package jar to war

--- src/main/java/com/mq/rabbitmq/RabbitMqApplication.java
+++ src/main/java/com/mq/rabbitmq/RabbitMqApplication.java
@@ -2,9 +2,16 @@ package com.mq.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RabbitMqApplication {
+public class RabbitMqApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RabbitMqApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqApplication.class, args);
